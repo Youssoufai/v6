d8ddaada8cdbdb25c4740ab6513eb1038517f6a2
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-07-08 22:35:27 +0100

    Commit

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::view('/about', 'about')->name('about');
+Route::view('/service', 'service')->name('service');
+Route::view('/project', 'project')->name('project');
