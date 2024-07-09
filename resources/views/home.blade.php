ccfeea0d238db7b142c7f70523dbb0c7dbacd92f
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-07-09 17:18:00 +0100

    Team

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -438,8 +438,8 @@
             <div class="container quote px-lg-0">
                 <div class="row g-0 mx-lg-0">
                     <div class="col-lg-6 ps-lg-0" style="min-height: 400px">
-                        <div class="position-relative h-100">
-                            <img class="position-absolute img-fluid w-100 h-100" src="img/quote.jpg"
+                        <div class="position-relative h-100 bg-dark">
+                            <img class="position-absolute img-fluid w-100 h-100" src="img/4.png"
                                 style="object-fit: cover" alt="" />
                         </div>
                     </div>
@@ -493,7 +493,7 @@
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="team-item">
                             <div class="overflow-hidden position-relative">
-                                <img class="img-fluid" src="img/team-1.jpg" alt="" />
+                                <img class="img-fluid" src="img/alamin.jpg" alt="" />
                                 <div class="team-social">
                                     <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
                                     <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
@@ -501,15 +501,15 @@
                                 </div>
                             </div>
                             <div class="text-center border border-5 border-light border-top-0 p-4">
-                                <h5 class="mb-0">Full Name</h5>
-                                <small>Designation</small>
+                                <h5 class="mb-0">Alamin Tajudeen Abbas</h5>
+                                <small>CEO</small>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="team-item">
                             <div class="overflow-hidden position-relative">
-                                <img class="img-fluid" src="img/team-2.jpg" alt="" />
+                                <img class="img-fluid" src="img/abba.jpg" alt="" />
                                 <div class="team-social">
                                     <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
                                     <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
@@ -517,7 +517,7 @@
                                 </div>
                             </div>
                             <div class="text-center border border-5 border-light border-top-0 p-4">
-                                <h5 class="mb-0">Full Name</h5>
+                                <h5 class="mb-0">Abba Ibrahim Jidda</h5>
                                 <small>Designation</small>
                             </div>
                         </div>
