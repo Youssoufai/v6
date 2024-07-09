52baf3c4c881973d3c011d7cf46c45ab6bd7d0d9
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-07-09 17:00:48 +0100

    Activities and Contact Section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -370,9 +370,9 @@
                 <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
                     <div class="rounded overflow-hidden">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="img/portfolio-1.jpg" alt="" />
+                            <img class="img-fluid w-100" src="img/summit.jpg" alt="" />
                             <div class="portfolio-overlay">
-                                <a class="btn btn-square btn-outline-light mx-1" href="img/portfolio-1.jpg"
+                                <a class="btn btn-square btn-outline-light mx-1" href="img/summit.jpg"
                                     data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                             </div>
                         </div>
@@ -388,30 +388,38 @@
                 <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.3s">
                     <div class="rounded overflow-hidden">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="img/portfolio-2.jpg" alt="" />
+                            <img class="img-fluid w-100" src="img/cup.jpg" alt="" />
                             <div class="portfolio-overlay">
-                                <a class="btn btn-square btn-outline-light mx-1" href="img/portfolio-2.jpg"
+                                <a class="btn btn-square btn-outline-light mx-1" href="img/cup.jpg"
                                     data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                             </div>
                         </div>
                         <div class="border border-5 border-light border-top-0 p-4">
-                            <p class="text-primary fw-medium mb-2">Demo 2</p>
-                            <h5 class="lh-base mb-0">Lorem ipsum dolor sit amet.</h5>
+                            <p class="text-primary fw-medium mb-2">Versa Cup: May 25th & 26th (2024)</p>
+                            <p class="lh-base mb-0">Earlier this year Versalink hosted the Versa Cup, a grassroots
+                                football tournament, on May 25th and 26th. Eight teams battled it out,showcasing their
+                                skills and sportsmanship. But the focus wasn't just on winning (congrats to the
+                                champions!). It was about fostering a love for football, teamwork, and community also to
+                                empower the youths and promote talents. </p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.5s">
                     <div class="rounded overflow-hidden">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="img/portfolio-3.jpg" alt="" />
+                            <img class="img-fluid w-100" src="img/bee.jpg" alt="" />
                             <div class="portfolio-overlay">
-                                <a class="btn btn-square btn-outline-light mx-1" href="img/portfolio-3.jpg"
+                                <a class="btn btn-square btn-outline-light mx-1" href="img/bee.jpg"
                                     data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                             </div>
                         </div>
                         <div class="border border-5 border-light border-top-0 p-4">
-                            <p class="text-primary fw-medium mb-2">Demo 3</p>
-                            <h5 class="lh-base mb-0">Lorem ipsum dolor sit amet.</h5>
+                            <p class="text-primary fw-medium mb-2">Versalink Organized Spelling Bee at Betharbel IDP
+                                School (Nov 28, 2023). </p>
+                            <p class="lh-base mb-0">The event ignited a love for learning among these talented
+                                students, highlighting the need for continued educational support. Versalink remains
+                                committed,to expand educational initiatives and empower IDP students to reach their full
+                                potential..</p>
                         </div>
                     </div>
                 </div>
@@ -438,12 +446,10 @@
                     <div class="col-lg-6 quote-text py-5 wow fadeIn" data-wow-delay="0.5s">
                         <div class="p-lg-5 pe-lg-0">
                             <div class="section-title text-start">
-                                <h1 class="display-5 mb-4">Be a Voluntee</h1>
+                                <h1 class="display-5 mb-4">Contact Us</h1>
                             </div>
                             <p class="mb-4 pb-2">
-                                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
-                                Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
-                                sed stet lorem sit clita duo justo erat amet
+                                Feel free to reach out to us
                             </p>
                             <form>
                                 <div class="row g-3">
@@ -459,16 +465,9 @@
                                         <input type="text" class="form-control border-0" placeholder="Your Mobile"
                                             style="height: 55px" />
                                     </div>
-                                    <div class="col-12 col-sm-6">
-                                        <select class="form-select border-0" style="height: 55px">
-                                            <option selected>Select A Service</option>
-                                            <option value="1">Service 1</option>
-                                            <option value="2">Service 2</option>
-                                            <option value="3">Service 3</option>
-                                        </select>
-                                    </div>
+
                                     <div class="col-12">
-                                        <textarea class="form-control border-0" placeholder="Special Note"></textarea>
+                                        <textarea class="form-control border-0" placeholder="Message"></textarea>
                                     </div>
                                     <div class="col-12">
                                         <button class="btn btn-primary w-100 py-3" type="submit">
